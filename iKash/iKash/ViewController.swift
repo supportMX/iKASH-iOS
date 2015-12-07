8684a751a4aef91b23f2f63f3440ab218227790c
supportMX <gaomengyuan@gaomengyuandemacbook-pro.local>
2015-12-07 17:56:10 +0800

    TestInit

---
+++ iKash/iKash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iKash
+//
+//  Created by 高梦缘 on 15/12/7.
+//  Copyright © 2015年 gocpa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
